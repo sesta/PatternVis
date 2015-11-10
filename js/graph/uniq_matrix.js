d374e50b534b2437bc5272683ec8f4c623785c5f
Sesta <sesta.ec5@gmail.com>
2015-11-10 13:19:09 +0900

    uniq もどうにかしました

--- js/graph/uniq_matrix.js
+++ js/graph/uniq_matrix.js
@@ -12,7 +12,7 @@ pattern_vis.View.prototype.uniq_matrixCreate = function(){
 }
 
 pattern_vis.View.prototype.uniq_matrixDraw = function(){
-  var that = this;
+  var self = this;
 
   var graph_width = this.svg_width - MARGIN.graph.left - MARGIN.graph.right;
   var graph_height = this.svg_height - MARGIN.graph.top - MARGIN.graph.bottom;
@@ -35,11 +35,11 @@ pattern_vis.View.prototype.uniq_matrixDraw = function(){
 
   var data = [];
   this.event_ids.forEach( function( event_id_1 ){
-    that.event_ids.forEach( function( event_id_2 ){
+    self.event_ids.forEach( function( event_id_2 ){
       data.push( {
         id_1: event_id_1,
         id_2: event_id_2,
-        value: Feature.get( that.feature_id, event_id_1, event_id_2 )
+        value: Feature.get( self.feature_id, event_id_1, event_id_2 )
       } );
     } );
   } );
@@ -64,8 +64,8 @@ pattern_vis.View.prototype.uniq_matrixDraw = function(){
     .data( data )
     .enter().append( "rect" )
     .attr( "class", function( d ){
-      if( that.event_history[ d.id_1 ] && ( d.id_1 == d.id_2 ) )
-        that.event_history[ d.id_1 ].to_d3_vis_val = d3.select( this );
+      if( self.event_history[ d.id_1 ] && ( d.id_1 == d.id_2 ) )
+        self.event_history[ d.id_1 ].to_d3_vis_val = d3.select( this );
       return "matrix vis-val event-id-" + d.id_1 + " event-id-" + d.id_2;
     } );
 
@@ -79,7 +79,7 @@ pattern_vis.View.prototype.uniq_matrixDraw = function(){
     .attr( "center-y", function( d ){ return y( d.id_2 ) + y.rangeBand() / 2.0; } )
     .attr( "fill", getGrayScale )
     .on( "click", function( d, i ){
-      Ui.click_vis_val( d3.select( this ), that );
+      Ui.click_vis_val( d3.select( this ), self );
     } )
     .on( "mouseover", function( d, i ){
       Ui.over_vis_val( d3.select( this ) );
@@ -87,6 +87,54 @@ pattern_vis.View.prototype.uniq_matrixDraw = function(){
     .on( "mouseout", function( d, i ){
       Ui.out_vis_val( d3.select( this ) );
     } );
+
+  var $selectable_area = self.$view.find( ".selectable-area" );
+  $selectable_area.children().remove();
+
+  var val_id = 0;
+  this.d3_graph.selectAll( ".vis-val" ).each( function( d ){
+    var $selectable_div = $( "<div></div>", {
+      "event-id": d.id_1 + "," + d.id_2,
+      "class": "event-id-" + d.id_1 + " event-id-" + d.id_2,
+      "center-y": $( this ).attr( "center-y" ),
+      "center-x": $( this ).attr( "center-x" ),
+      "val-id": val_id
+    } ).css( {
+        top: ( MARGIN.graph.top + $( this ).attr( "y" ) * 1.0 ) + "px",
+        left: ( MARGIN.graph.left + $( this ).attr( "x" ) * 1.0 ) + "px",
+        height: $( this ).attr( "height" ) + "px",
+        width: $( this ).attr( "width" ) + "px"
+      } ).on( "mouseover", function(){
+        Ui.over_vis_val( $( this ) );
+      } ).on( "mouseout", function(){
+        Ui.out_vis_val( $( this ) );
+      } );
+
+    d3.select( this ).classed( "val-id-" + val_id, true );
+
+    val_id++;
+
+    $selectable_area.append( $selectable_div );
+  } );
+
+  $selectable_area.selectable( {
+    selecting: function( event, ui ){
+      $( this ).children( ".ui-selecting" )
+        .each( function(){
+          Ui.select_vis_val( $( this ), self );
+        } );
+    },
+    stop: function( event, ui ){
+      if( $( this ).children( ".ui-selected" ).length == 0 ){
+        Ui.cancel_selecting();
+      }
+    }
+  } );
+
+  function getGrayScale( d ){
+    var gray_scale = 255 - parseInt( 255 * d.value / max_value, 10 );
+    return "rgb( " + gray_scale + "," + gray_scale + "," + gray_scale + " )";
+  }
 
   function getGrayScale( d ){
     var gray_scale = 255 - parseInt( 255 * d.value / max_value, 10 );
