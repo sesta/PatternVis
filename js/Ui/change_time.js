1c5fd6b4246c2080c7989c1b6773a04e71b4002b
Sesta <sesta.ec5@gmail.com>
2015-11-20 16:46:09 +0900

    リアルタイムに時刻が変わるようにしました

--- js/Ui/change_time.js
+++ js/Ui/change_time.js
@@ -1,14 +1,23 @@
 $( function(){
+  $( ".start-time-slider" ).on( "input", function(){
+    $( ".start-time-span" )
+      .text( d3.time.format( "%Y/%m/%d" )( new Date( $( this ).val() * 1.0 ) ) );
+  } );
+
+  $( ".end-time-slider" ).on( "input", function(){
+    $( ".end-time-span" )
+      .text( d3.time.format( "%Y/%m/%d" )( new Date( $( this ).val() * 1.0 ) ) );
+  } );
+
   $( ".start-time-slider" ).on( "change", function(){
     setting.time.start = new Date( $( this ).val() * 1.0 );
 
     if( setting.time.start > setting.time.end ){
       setting.time.start = setting.time.end;
       $( this ).val( setting.time.end.getTime() );
-    }
 
-    $( ".start-time-span" )
-      .text( d3.time.format( "%Y/%m/%d" )( setting.time.start ) );
+      return;
+    }
 
     overview.filterRecordsByTime();
   } );
@@ -19,10 +28,9 @@ $( function(){
     if( setting.time.end < setting.time.start ){
       setting.time.end = setting.time.start;
       $( this ).val( setting.time.start.getTime() );
-    }
 
-    $( ".end-time-span" )
-      .text( d3.time.format( "%Y/%m/%d" )( setting.time.end ) );
+      return;
+    }
 
     overview.filterRecordsByTime();
   } );
